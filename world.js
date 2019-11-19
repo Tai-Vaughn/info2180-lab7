d38c0e18f6b3a63cde2cd0888b890a090c2b59db
Tai-Vaughn White <Isamutai200@gmail.com>
2019-11-19 10:08:12 -0500

    Returning a Table from php

--- world.js
+++ world.js
@@ -2,15 +2,15 @@ window.onload = function() {
     var lookup = document.getElementById('lookup');
     var httpRequest = new XMLHttpRequest();
     var url = 'world.php?country='
-    
+    let countries;
     lookup.addEventListener('click' , function(){
-        var result = document.getElementById('result');
         var searchOption = document.getElementById('country').value;
         httpRequest.open('GET',url+searchOption) 
         httpRequest.send();
         httpRequest.onload =function(){
             if(this.readyState == 4 && this.status == 200){
-                alert(this.responseText)
+                var result = document.getElementById('result');
+                result.innerHTML = this.responseText
             }
         }
         
--- world.php
+++ world.php
@@ -5,12 +5,34 @@
   $dbname = 'world';
 
   $conn = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8mb4", $username, $password);
-  $stmt = $conn->prepare("SELECT * FROM countries Where name = :country");
-  $country = filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+  $country = $_GET['country'];//filter_input(INPUT_GET, 'country', FILTER_SANITIZE_STRING);
+  $stmt = $conn->prepare("SELECT * FROM countries Where name LIKE '%$country%' ");
   $stmt->bindParam(':country' , $country , PDO::PARAM_STR);
   $stmt->execute();
   $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  echo json_encode($results);
+  //+echo json_encode($results);
 
  ?>
+
+<table id="table">
+  <thead>
+    <tr>
+      <th>Name</th>
+      <th>Continent</th>
+      <th>Independence</th>
+      <th>Head of State</th>
+    </tr>
+  </thead>
+  <tbody >
+    <?php foreach($results as $row):?>
+    <tr>
+      <td><?= $row['name'];?></td>
+      <td><?= $row['continent'];?></td>
+      <td><?= $row['independence_year'];?></td>
+      <td><?= $row['head_of_state'];?></td>
+    </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
+
 
